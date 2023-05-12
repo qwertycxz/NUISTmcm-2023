5e02932caa31bb4728e92372a7958c22d046698b
qwertycxz <gmail2@qq.com>
2023-05-12 19:21:10 +0800

    第一问进行到第五步

--- B题.r
+++ B题.r
@@ -1,6 +1,6 @@
 library("psych")
 library("nFactors")
-# 第一问
+# 第一问:完成?
 x_train <- read.csv("X_train.csv")
 x_coefficient <- corr.test(x_train) # 警告: 这个函数会执行大约15min!
 # 将得到的所有数据进行输出
@@ -28,8 +28,10 @@ plotnScree(x_factor_number)
 x_coefficient_r <- read.csv("x_coefficient_r.csv")
 x_matrix <- as.matrix(x_coefficient_r[, 2:ncol(x_coefficient_r)])
 row.names(x_matrix) <- x_coefficient_r[, 1]
-x_factor_7 <- fa(x_matrix, 7, fm = "pa", rotate = "varimax") #TODO: 因子个数与旋转方法有待商榷
+x_factor_7 <- fa(x_matrix, 100, fm = "pa", rotate = "varimax")
+x_factor_7 <- fa(x_matrix, 107, fm = "pa", rotate = "varimax")
 write.csv(x_factor_7$loadings, "x_loadings.csv")
+write.csv(x_factor_7$Vaccounted, "x_vaccount.csv")
 x_factor_7
 plot(x_factor_7$loadings, type = "n")
 text(x_factor_7$loadings, labels = names(x_train))
